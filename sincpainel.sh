1ae1b20c45a853601e437b987abdf4215eb8ddc7
Dealer Services <159734968+Dealer-Dev@users.noreply.github.com>
2025-02-23 21:22:04 -0400

    Update sincpainel.sh

--- sincpainel.sh
+++ sincpainel.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 clear
-echo -e "\033[1;36mINSTALANDO O SINCRONIZADOR SSH WEB\033[0m"
+echo -e "\033[1;36mINSTALANDO SINCRONIZADOR SSH WEB\033[0m"
 echo "America/Sao_Paulo" > /etc/timezone
 ln -fs /usr/share/zoneinfo/America/Sao_Paulo /etc/localtime
 dpkg-reconfigure --frontend noninteractive tzdata
@@ -8,7 +8,7 @@ clear
 echo -e "\033[1;36mPOR FAVOR AGUARDE...\033[0m"
 crack=$(cut -d"'" -f2 /opt/sshplus/licenca.txt) > /dev/null 2>&1
 sleep 1.5
-echo -e "\033[1;36mREGISTRANDO UMA LICENÇA...\033[0m"
+echo -e "\033[1;36m🐇 REGISTRANDO LICENCIA 🐇\033[0m"
 rm *.sh* > /dev/null 2>&1
 rm *.zip > /dev/null 2>&1
 apt install unzip -y > /dev/null 2>&1
@@ -30,7 +30,7 @@ chmod +x *sh > /dev/null 2>&1
 service ssh restart
 echo -e "\n\033[1;32mCONCLUÍDO!\033[0m"
 sleep 15
-echo -e "\033[1;36mENTRE NO PAINEL E CRIE UMA CONTA SSH PRA VER SE ESTA TUDO OK!\033[0m"
+echo -e "\033[1;36mENTRE AL PAINEL Y CREE UNA CUENNTA SSH PARA VER SI ESTÁ TODO BIEN!\033[0m"
 sleep 5
 cat /dev/null > ~/.bash_history && history -c && clear
 rm sincpainel* > /dev/null 2>&1
